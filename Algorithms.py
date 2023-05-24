a134f5bee1b7b8bc8a0826536c50ee6a9e4010d5
Sarah <cmcgriffith@gmail.com>
2023-05-24 16:34:42 +0300

    bug fixes

--- Algorithms.py
+++ Algorithms.py
@@ -3,6 +3,7 @@ import numpy as np
 from FrozenLakeEnv import FrozenLakeEnv
 from typing import List, Tuple
 import heapdict
+import math
 
 class Node():
     def __init__(self, id, cost, h_cost = 0):
@@ -212,6 +213,13 @@ class GreedyAgent(Agent):
                         openNodes[child] = (child.h_cost, child.id)
         return [None, -1, -1]
 
+class NodeWA(Node):
+    def __init__(self, id, cost, m_cost, g_cost, h_weight):
+        self.m_cost = m_cost*h_weight
+        self.g_cost = g_cost
+        Node.__init__(self, id, cost, self.m_cost + (self.g_cost*(1-h_weight)))
+        return None
+
 class WeightedAStarAgent(Agent):
     
     def __init__(self):
@@ -219,34 +227,28 @@ class WeightedAStarAgent(Agent):
         self.h_weight = None
         return None
 
-    def h_cost(self, node, previous = None, cost = 0) -> int:
-        h = super().h_cost(node)
-        g = self.get_cost(previous) + cost
-        return ((1-self.h_weight)*g + self.h_weight*h)
-
     def search(self, env: FrozenLakeEnv, h_weight) -> Tuple[List[int], float, int]:
         self.env = env
         self.h_weight = h_weight
         self.env.reset()
-        state = Node(self.env.get_initial_state(), 0, self.h_cost(self.env.get_initial_state()))
+        state = NodeWA(self.env.get_initial_state(), 0, self.h_cost(self.env.get_initial_state()), 0, self.h_weight)
         open = heapdict.heapdict()
         close = {}
         openIDs = {}
         open[state.id] = (state.h_cost, state.id)
         openIDs[state.id] = state
         while open:
-            key = open.popitem()[0]
-            state = openIDs.pop(key)
-            self.env.set_state(state.id)
+            state = openIDs.pop(open.popitem()[0])
             close[state.id] = state.h_cost
-            if self.env.is_final_state(state.id):
-                return [self.solution(state), self.cost, self.expanded]
             if state.id != None:
+                self.env.set_state(state.id)
+                if self.env.is_final_state(state.id):
+                    return [self.solution(state), self.cost, self.expanded]
                 self.add_expansion()
                 for action, successor in (self.env.succ(self.env.get_state())).items():
                     if action == None:
                         break
-                    child = Node(successor[0], successor[1], self.h_cost(successor[0], state, successor[1]))
+                    child = NodeWA(successor[0], successor[1], self.h_cost(successor[0]), (state.g_cost + successor[1]), self.h_weight)
                     if child.id not in close.keys() and child.id not in openIDs.keys():
                         child.set_predecessor(state, action)
                         open[child.id] = (child.h_cost, child.id)
@@ -254,14 +256,15 @@ class WeightedAStarAgent(Agent):
                     elif child.id in openIDs.keys():
                         if open[child.id][0] > child.h_cost:
                             child.set_predecessor(state, action)
+                            open.__delitem__(child.id)
                             open[child.id] = (child.h_cost, child.id)
+                            openIDs.__delitem__(child.id)
                             openIDs[child.id] = child
                     elif child.h_cost < close[child.id]:
                         child.set_predecessor(state, action)
                         open[child.id] = (child.h_cost, child.id)
                         openIDs[child.id] = child
-                        close.pop(child.id)
-                        self.add_expansion(-1) #to avoid double-counting expanding the same node
+                        close.__delitem__(child.id)
         return [None, -1, -1]
 
 
@@ -278,7 +281,7 @@ class IDAStarAgent(Agent):
             self.limit = min(self.limit, new_f)
             return [None, -1, -1]
         if self.env.is_final_state(state.id):
-            return [self.path.values(), cost, -1]
+            return [list(self.path.values()), cost, -1]
         for action, successor in (self.env.succ(state.id)).items():
             if action == None:
                 break
@@ -301,7 +304,7 @@ class IDAStarAgent(Agent):
         flag = True
         while flag:
             f_limit = self.limit
-            self.limit = 1000
+            self.limit = math.inf
             result = self.DFSf(state, cost, f_limit)
             if result != [None, -1, -1]:
                 return result
